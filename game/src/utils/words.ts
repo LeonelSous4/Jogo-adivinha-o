a4cf5722c82cac7f0d1821efdf9acc663c77a06d
Leonel Sousa <leonel.sousa.0707@gmail.com>
2026-09-25 16:08:15 -0300

    iniciando projetopratico react game

---
+++ game/src/utils/words.ts
@@ -0,0 +1,18 @@
+export type Challenge = {
+    id: number 
+    word: string
+    tip: string
+}
+
+export const WORDS: Challenge[] = [
+    {id: 1 , word: "CSS", tip: "Linguagem de estilos"},
+    {id: 2 , word: "REACT", tip: "Biblioteca interfaces web"},
+    {id: 3 , word: "HTML", tip: "Linguagem de marcação"},
+    {
+        id: 4, 
+        word: "Java Script", 
+        tip: "Linguagem de programação"
+    },
+    {id: 5 , word: "Type Script", tip: "Adicionar tipagem ao js"},
+
+]
